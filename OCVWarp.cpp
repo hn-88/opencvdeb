08e8c71464540be4ca89909ead4f502ee712dd02
hn-88 <hn-88@users.noreply.github.com>
2020-05-09 15:22:02 +0530

    for dummy vcxproj

---
+++ OCVWarp.cpp
@@ -0,0 +1,13 @@
+// Just a dummy file for dummy.vcxproj
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	 
+  std::cout << std::endl << "Finished." << std::endl;
+    return 0;
+	   
+	   
+} // end main
